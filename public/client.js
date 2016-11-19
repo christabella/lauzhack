2022015de34ad0bfb41f0f5ac1706c5bb6badaed
Christabella Irwanto <christabella.irwanto@gmail.com>
2016-11-19 20:27:46 +0100

    auto-crop on transparent images

--- public/client.js
+++ public/client.js
@@ -50,24 +50,63 @@ else if (navigator.getUserMedia) { // Standard
 document.getElementById('snap').addEventListener('click', function() {
     //var fullQuality = canvas.toDataURL("image/jpeg", 1.0);
 
-    var fullQualityImg = canvas.toDataURL("img/png", 1.0); // Data URI
     var originalImg = new Image;
-    
-    context.drawImage(video, 0, 0, 640, 480); // Draw video screenshot in canvas
-    var imgData = context.getImageData(0, 0, canvas.width, canvas.height); // Get image data from canvas context
+
+    // Draw video screenshot in canvas
+    context.drawImage(video, 0, 0, 640, 480); 
+    // Create PNG image to upload
+    var fullQualityImg = canvas.toDataURL("img/png", 1.0); 
+    // Get image data from canvas context, and get its raw pixel data
+    var imgData = context.getImageData(0, 0, canvas.width, canvas.height); 
     var data = imgData.data;
     $.post('/upload', {
         img : fullQualityImg
     })
     console.log(data);
+    // Turn blue bits transparent
     for (var i = 0; i < data.length; i += 4) {
         // data: one-dimensional array containing the data in the RGBA order
         if (data[i] >= 75 && data[i] <=76 && data[i + 1] == 65 && data[i + 2] >= 252) {
-            // red within [75, 76], green within [65], blue within [252, 254]
+            // These 4 values make up a blue pixel: red within [75, 76], green within [65], blue within [252, 254]
             data[i + 3] = 0; // set alpha value to 0
         }
     }
-    console.log(data);
-    context.putImageData(imgData, 0, 0);
+    console.log(data); // Data is now partially transparent
+
+    // Auto-crop the image (imgData)
+    var w = canvas.width,
+        h = canvas.height,
+    pix = {x:[], y:[]},
+    x, y, index;
+
+    for (y = 0; y < h; y++) {
+        for (x = 0; x < w; x++) {
+            index = (y * w + x) * 4;
+            if (imgData.data[index+3] > 0) {
+
+                pix.x.push(x);
+                pix.y.push(y);
+
+            }   
+        }
+    }
+    pix.x.sort(function(a,b){return a-b});
+    pix.y.sort(function(a,b){return a-b});
+    var n = pix.x.length-1;
+
+    w = pix.x[n] - pix.x[0];
+    h = pix.y[n] - pix.y[0];
+
+    // Cut from context
+    var cut = context.getImageData(pix.x[0], pix.y[0], w, h);
+
+    // Resize canvas to fit new cropped dimensions
+    canvas.width = w;
+    canvas.height = h;
+    context.putImageData(cut, 0, 0);
+
+    // Get PNG
+    // var image = canvas.toDataURL();
+
 
 });
